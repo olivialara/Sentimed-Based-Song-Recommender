3375e82cc959d726feda8832c01ba9ee745d5be3
Olivia Lara <oliviamarielara@gmail.com>
2021-12-28 21:53:16 -0800

    Update III-Application.py

--- code/III-Application.py
+++ code/III-Application.py
@@ -22,7 +22,7 @@ I ain't ever trapped out the bando
 But oh Lord, don't get me wrong, (...)
 ''')
 
-small_data = pd.read_pkl('data/vadered&no_lyrics.pkl')
+small_data = pd.read_pkl('data/vadered&no_lyrics.pkl', compression={'method':'zip'})
 
 
 def song_recs (sentence):
